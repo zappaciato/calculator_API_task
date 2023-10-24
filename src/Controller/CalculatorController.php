7ea51a764ef5ac33f85968a82d0cd198a275a475
zappaciato <58880515+zappaciato@users.noreply.github.com>
2023-10-24 09:35:43 +0200

    First functionalities setup and tested.

---
+++ src/Controller/CalculatorController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\CalculatorService;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CalculatorController extends AbstractController
+{
+    public function __construct(private readonly CalculatorService $calculatorService)
+    {
+
+    }
+
+
+    #[Route('/add/{operand_x}/{operand_y}', name: 'add_calculator', methods: ['POST'])]
+    public function add($operand_x, $operand_y): JsonResponse
+    {
+        $result = $this->calculatorService->add($operand_x, $operand_y);
+
+        return $this->json([
+            $result
+        ]);
+    }
+
+    #[Route('/subtract/{operand_x}/{operand_y}', name: 'subtract_calculator', methods: ['POST'])]
+    public function subtract($operand_x, $operand_y): JsonResponse
+    {
+        $result = $this->calculatorService->subtract($operand_x, $operand_y);
+
+        return $this->json([
+            $result
+        ]);
+    }
+
+    #[Route('/multiply/{operand_x}/{operand_y}', name: 'multiply_calculator', methods: ['POST'])]
+    public function multiply($operand_x, $operand_y): JsonResponse
+    {
+        $result = $this->calculatorService->multiply($operand_x, $operand_y);
+
+        return $this->json([
+            $result
+        ]);
+    }
+
+    #[Route('/divide/{operand_x}/{operand_y}', name: 'divide_calculator', methods: ['POST'])]
+    public function divide($operand_x, $operand_y): JsonResponse
+    {
+        $result = $this->calculatorService->divide($operand_x, $operand_y);
+
+        return $this->json([
+            $result
+        ]);
+    }
+}
